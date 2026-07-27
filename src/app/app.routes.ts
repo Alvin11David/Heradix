8bade771b1680061d3dd8a52c55bc6548b3a6ad0
Waluube Alvin David <alvin69david@gmail.com>
2026-07-27 11:28:32 +0000

    worked on the width

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
 
   {
     path: 'auth',
-    canActivate: [guestGuard],
+    // canActivate: [guestGuard],
     children: [
       { path: 'login',            loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
       { path: 'register',         loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) },
